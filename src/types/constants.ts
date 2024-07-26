844de6f43ed5895bae1c593e936c061132115ceb
Chris Wade <genghishack@gmail.com>
2024-07-25 18:50:38 -0600

    added new tables to defaults and types

--- src/types/constants.ts
+++ src/types/constants.ts
@@ -35,9 +35,36 @@ export type Tables = {
   staging: string;
   category: string;
   professional: string;
+  organization: string;
+  address: string;
+  specialty: string;
+  topic: string;
+  pub: string;
+  comment: string;
+  prof_x_org: string;
   prof_x_cat: string;
+  prof_x_addr: string;
+  prof_x_spec: string;
+  prof_x_topic: string;
+  prof_x_pub: string;
+  prof_x_comment: string;
   prof_deleted: string;
-  prof_geom: string;
+  prof_phone: string;
+  prof_email: string;
+  prof_social: string;
+  prof_url: string;
+  prof_bar: string;
+  org_x_cat: string;
+  org_x_addr: string;
+  org_x_spec: string;
+  org_x_pub: string;
+  org_x_comment: string;
+  org_deleted: string;
+  org_phone: string;
+  org_email: string;
+  org_social: string;
+  org_url: string;
+  address_geom: string;
 }
 
 export type Constants = {
